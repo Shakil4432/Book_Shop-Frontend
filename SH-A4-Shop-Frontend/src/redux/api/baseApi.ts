a5c07fceaba60bd6dfca2c1a1c6b71a26e3ff7c5
Shakil4432 <shakil4432@gmail.com>
2025-03-14 19:12:18 +0600

    d

--- SH-A4-Shop-Frontend/src/redux/api/baseApi.ts
+++ SH-A4-Shop-Frontend/src/redux/api/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5000/api",
+    baseUrl: "https://stationary-a4-shop-backend-six.vercel.app/api",
     credentials: "include",
   }),
   tagTypes: ["Book", "User", "Order"],
